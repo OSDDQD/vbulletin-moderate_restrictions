69a275e4701458b5dffb2865f1cee6a1225ba37d
Slowpoked <slowpokedme@gmail.com>
2015-07-08 14:41:22 +0500

    Release

--- includes/class_modrestriction.php
+++ includes/class_modrestriction.php
@@ -4,55 +4,78 @@ if (!isset($GLOBALS['vbulletin']->db))
 	exit;
 }
 
-class moderateRestriction {
+class moderateRestriction 
+{
 
-	private $registry;
-	private $response;
-	private $day;
+	var $registry = null;
+	var $day = null;
 
 	public function __construct ($registry) 
 	{
 		$this->registry = $registry;
-		$this->response = $response;
-		$this->day = $day = strtotime('1 day', 0);
+		$this->day = strtotime('1 day', 0);
 	}
 
-	public static function verify($userid, $range, $type, $max) 
+	public function verify($userid, $range, $type, $limit) 
 	{
-		$data = array(
+		$count = self::getData(array(
 			"userid" => $userid,
 			"range"	 => $range,
 			"type"	 => $type
-		);
+		));
 
-		return self::getData($data);
+		if($count >= $limit)
+			switch ($type) {
+				case 'post':
+					$result = array(
+							"error" => "modrestriction_limit_post",
+							"range"	=> $range,
+							"limit"	=> $limit
+						);
+					break;
+				
+				case 'infraction':
+					$result = array(
+							"error" => "modrestriction_limit_infraction",
+							"range"	=> $range,
+							"limit"	=> $limit
+						);
+					break;
+			}
+
+			return $result;
+
+		return false;
  	}
 
- 	private static function getData($data = array())
+ 	public function getData($data = array())
  	{
- 		$rangeStart = TIMENOW - ($day * $data['range']);
+ 		$rangeStart = TIMENOW - ($this->day * $data['range']);
 
  		switch ($data['type']) {
  			case 'post':
- 				$response = $registry->db->query_read("
- 					SELECT COUNT(primaryid) FROM " . TABLE_PREFIX . "deletionlog
+ 				$response = $this->registry->db->query_read("
+ 					SELECT * FROM " . TABLE_PREFIX . "deletionlog
  					WHERE userid = " . $data['userid'] . "
+ 					AND type = 'post'
  					AND dateline >= " . $rangeStart . "
  					AND dateline <= " . TIMENOW . "
 				");
  				break;
  			
- 			case 'infraction'
- 				$response = $registry->db->query_read("
- 					SELECT COUNT(infractionid) FROM " . TABLE_PREFIX . "infraction
+ 			case 'infraction':
+ 				$response = $this->registry->db->query_read("
+ 					SELECT * FROM " . TABLE_PREFIX . "infraction
  					WHERE whoadded = " . $data['userid'] . "
+ 					AND action <> 2
+ 					AND actionuserid <>  " . $data['userid'] . "
  					AND dateline >= " . $rangeStart . "
  					AND dateline <= " . TIMENOW . "
 				");
  				break;
  		}
 
- 		return $registry->db->fetch_array($response);
+ 		return $this->registry->db->num_rows($response);
 
  	}
 }
